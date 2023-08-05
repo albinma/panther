f5c2887f4471667a9088490259b11257c2d7e116
Albin Ma <albinma@gmail.com>
2023-08-04 23:49:57 -0400

    Add next-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import Header from '@/components/Header/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from './providers';
 import cn from 'classnames';
+import Providers from '@/app/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className={cn(inter.className, 'dark:bg-black bg-white')}>
         <Providers>
           <Header />
