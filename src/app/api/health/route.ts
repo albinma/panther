aa9b0070738badee9624d02510f8e569baee9fd0
Albin Ma <albinma@gmail.com>
2023-08-09 01:47:52 -0400

    Fuck off

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -4,14 +4,11 @@ import { NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
 export async function GET(): Promise<NextResponse> {
-  try {
-    const userCount = await prisma.user.count();
-    if (userCount <= 0) {
-      throw new Error('No users found, seed failed');
-    }
-
-    return NextResponse.json({ status: 'healthy' });
-  } catch (error) {
-    return NextResponse.json({ status: 'error', error });
+  const userCount = await prisma.user.count();
+  throw new Error('test');
+  if (userCount <= 0) {
+    throw new Error('No users found, seed failed');
   }
+
+  return NextResponse.json({ status: 'healthy' });
 }
