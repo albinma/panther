bf25f4b552157dbc96e02857761c2877c72530a3
Albin Ma <albinma@gmail.com>
2023-08-08 23:28:03 -0400

    Add seed example

---
+++ prisma/seed.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      jobTitle: 'developer',
+    },
+  });
+
+  const bob = await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      jobTitle: 'tester',
+    },
+  });
+
+  console.log({ alice, bob });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+
+  .catch(async (e) => {
+    console.error(e);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
