5cbc4a9c30613dc3b6e3596d1cede2c020d2883c
Albin Ma <albinma@gmail.com>
2023-08-09 01:53:58 -0400

    fuck

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(): Promise<NextResponse> {
   const userCount = await prisma.user.count();
 
