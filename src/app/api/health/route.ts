ea7391167a49b3850c04f29e3a787f4f5fedb73c
Albin Ma <albinma@gmail.com>
2023-08-09 01:49:36 -0400

    Fix that

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export async function GET(): Promise<NextResponse> {
   const userCount = await prisma.user.count();
-  throw new Error('test');
+
   if (userCount <= 0) {
     throw new Error('No users found, seed failed');
   }
