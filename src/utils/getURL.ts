b0b38e8ffa612128892becdbf612b3e9c1b2d6c7
Albin Ma <albinma@gmail.com>
2023-08-09 00:58:29 -0400

    Try fixing again

--- src/utils/getURL.ts
+++ src/utils/getURL.ts
@@ -1,7 +1,7 @@
 const IS_SERVER = typeof window === 'undefined';
 export default function getURL(path: string): string {
   const baseURL = IS_SERVER
-    ? process.env.NEXT_PUBLIC_SITE_URL ?? process.env.VERCEL_URL
+    ? process.env.NEXT_PUBLIC_BASE_URL
     : window.location.origin;
 
   return new URL(path, baseURL).toString();
