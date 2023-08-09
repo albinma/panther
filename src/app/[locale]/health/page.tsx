26ab4b51e1ee96e9a6a041d3c10aa40b20a817c0
Albin Ma <albinma@gmail.com>
2023-08-09 01:06:44 -0400

    Maybe this?

--- src/app/[locale]/health/page.tsx
+++ src/app/[locale]/health/page.tsx
@@ -1,26 +1,10 @@
-import getURL from '@/utils/getURL';
-
-async function getData(): Promise<{ status: string }> {
-  const res = await fetch(getURL('/api/health'));
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
-
-  const data = await res.json();
-
-  return data as { status: string };
-}
+import HealthStatus from '@/app/[locale]/health/HealthStatus';
 
 export default async function HealthPage(): Promise<JSX.Element> {
-  const data = await getData();
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-black dark:text-slate-50 transition-colors duration-150">
       <h1>Health</h1>
-      <p data-testid="health-status">{data.status}</p>
+      <HealthStatus />
     </div>
   );
 }
