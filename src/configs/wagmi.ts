f8bbe118a44c539c59e2f3dc4b409f65192b24f5
Albin Ma <albinma@gmail.com>
2023-09-27 00:20:39 -0400

    Install wagmi

---
+++ src/configs/wagmi.ts
@@ -0,0 +1,22 @@
+import { configureChains, createConfig, mainnet, sepolia } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, sepolia],
+  [publicProvider()],
+);
+
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    // Only support metamask for now
+    new MetaMaskConnector({
+      chains,
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+});
+
+export default config;
